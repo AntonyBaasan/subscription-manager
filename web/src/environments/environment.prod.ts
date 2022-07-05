3636290f82d0e05268a6b4f7ffdbde15dce9a8cf
Antony Baasandorj <antony.baasan@gmail.com>
2022-07-05 05:09:32 -0400

    add ref to cognito

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  cognito: {
+    userPoolId: 'us-east-1_H5pi1MIIK',
+    userPoolWebClientId: '6jl5lcvbjj0nnvfeii5t9gtonp'
+  }
 };
