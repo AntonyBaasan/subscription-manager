3ea20a46a0724e75f83cc5564124ed87491e872e
Antony B <antony.baasan@gmail.com>
2022-09-22 15:48:59 -0400

    test

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -5,5 +5,5 @@ import * as functions from "firebase-functions";
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase, from Test1!");
+  response.send("Hello from Firebase, from Test2!");
 });
