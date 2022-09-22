b81e137bab361420e00452f8501e3eeef5f8932a
Antony B <antony.baasan@gmail.com>
2022-09-22 19:46:39 -0400

    try query val

--- backend/functions/src/services/hello-world.service.ts
+++ backend/functions/src/services/hello-world.service.ts
@@ -1,10 +1,10 @@
 import * as functions from 'firebase-functions';
 
 
-export const helloWorldFunc = (
-  request: functions.Request,
-  response: functions.Response
+export const helloWorldFunc = async (
+  req: functions.Request,
+  res: functions.Response
 ) => {
   functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase, from Test 6 !' + JSON.stringify(request));
+  res.send('Hello from Firebase, from Test 6 !' + req.query.text);
 };
